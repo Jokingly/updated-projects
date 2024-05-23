411f009c2a08a9050e59e1a62e1bbcf3ae94455d
Jokingly <93153873+Jokingly@users.noreply.github.com>
2024-05-23 12:08:04 +0100

    debug - fixed buttons to hide/show forms with .matches()

--- fitness app/static/scripts.js
+++ fitness app/static/scripts.js
@@ -5,14 +5,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     document.documentElement.addEventListener("click", (e) => {
-        const targetClassName = e.target.className;
-        // helper functions
-
-        // check string for substring. boolean output
-        function checkSubString(mainString, subString) {
-            return mainString.indexOf(subString) !== -1;
-        }
-
 
         // visibility toggles for following edit fields:
         // - datetime
@@ -22,17 +14,17 @@ document.addEventListener("DOMContentLoaded", function() {
         // switch statement for readability
         switch(true) {
             // datetime edit fields
-            case checkSubString(targetClassName, 'edit-datetime-button'):
+            case e.target.matches('.edit-datetime-button, .edit-datetime-button *'):
                 document.getElementById('edit-datetime-fields').classList.toggle('hide');
                 break;
 
             // note edit field
-            case checkSubString(targetClassName, 'edit-note-button'):
+            case e.target.matches('.edit-note-button, edit-note-button *'):
                 document.getElementById('note-form').classList.toggle('hide');
                 break;
 
             // add set fields
-            case checkSubString(targetClassName, 'add-set-button'):
+            case e.target.matches('.add-set-button'):
                 const targetElements = document.getElementsByClassName(e.target.value);
 
                 for (let element of targetElements) {
@@ -41,13 +33,17 @@ document.addEventListener("DOMContentLoaded", function() {
                 break;
 
             // edit set fields
-            case checkSubString(targetClassName, 'edit-set-button'):
-                const targetElems = document.getElementsByClassName(e.target.value);
+            case e.target.matches('.edit-set-button, .edit-set-button *'):
+                let targetElems = document.getElementsByClassName(e.target.value);
+                
+                if (e.target.value === undefined) {
+                    targetElems = document.getElementsByClassName(e.target.parentNode.value);
+                }
+
                 for(let element of targetElems) {
                     element.classList.toggle('hide');
                 }
                 break;
-
         }
     });
 });
@@ -96,64 +92,64 @@ document.addEventListener("DOMContentLoaded", function() {
 //     fetchExercises();
 
 
-    // load list data into element's inner HTML
-    function loadData(data, element) {
-        if (data) {
-            // clearing element allows for loading up-to-date data without appending
-            element.innerHTML = "";
-            let innerElement = "";
+//     // load list data into element's inner HTML
+//     function loadData(data, element) {
+//         if (data) {
+//             // clearing element allows for loading up-to-date data without appending
+//             element.innerHTML = "";
+//             let innerElement = "";
 
-            // add first 5 data items to innerElement
-            for (let i = 0; i < 5; i++) {
-                // do not show "undefined"
-                if (data[i] === undefined) {
-                    continue;
-                }
-                innerElement += `<li>${data[i]}</li>`;
-            }
+//             // add first 5 data items to innerElement
+//             for (let i = 0; i < 5; i++) {
+//                 // do not show "undefined"
+//                 if (data[i] === undefined) {
+//                     continue;
+//                 }
+//                 innerElement += `<li>${data[i]}</li>`;
+//             }
 
-            // // add all data items to innerElement
-            // data.forEach((item) => {
-            //     innerElement += `<li>${item}</li>`;
-            // });
+//             // // add all data items to innerElement
+//             // data.forEach((item) => {
+//             //     innerElement += `<li>${item}</li>`;
+//             // });
 
-            element.innerHTML = innerElement;
+//             element.innerHTML = innerElement;
 
-            // enable users to click suggestions
-            const listElements = document.querySelectorAll("#exercise-list li");
-            listElements.forEach((element) => element.setAttribute("onclick", `displayName("${element.innerHTML}")`));
-            console.log(`displayName(${element.innerHTML})`);
+//             // enable users to click suggestions
+//             const listElements = document.querySelectorAll("#exercise-list li");
+//             listElements.forEach((element) => element.setAttribute("onclick", `displayName("${element.innerHTML}")`));
+//             console.log(`displayName(${element.innerHTML})`);
 
-        }
-    }
-
-
-    // filter user input (searchText)
-    function filterData(data, searchText) {
-        return data.filter((x) => x.toLowerCase().includes(searchText.toLowerCase()));
-    }
+//         }
+//     }
 
 
-    // EventListener
-    exerciseInputElement.addEventListener("input", function() {
-        // clears autocomplete suggestions at every function call, cleaning previous output
-        removeElements(exerciseListElement);
-
-        // show autocomplete suggestions, when input box is not empty
-        if (exerciseInputElement.value != "") {
-            // store list of filtered data in variable
-            const filteredData = filterData(exercises, exerciseInputElement.value);
-            // pass filtered data back into loadData to update "autocomplete suggestions"
-            loadData(filteredData, exerciseListElement);
-        }
-
-    });
+//     // filter user input (searchText)
+//     function filterData(data, searchText) {
+//         return data.filter((x) => x.toLowerCase().includes(searchText.toLowerCase()));
+//     }
 
 
-    // clear list of autocomplete suggestions
-    function removeElements(element) {
-        element.innerHTML = "";
-    }
+//     // EventListener
+//     exerciseInputElement.addEventListener("input", function() {
+//         // clears autocomplete suggestions at every function call, cleaning previous output
+//         removeElements(exerciseListElement);
+
+//         // show autocomplete suggestions, when input box is not empty
+//         if (exerciseInputElement.value != "") {
+//             // store list of filtered data in variable
+//             const filteredData = filterData(exercises, exerciseInputElement.value);
+//             // pass filtered data back into loadData to update "autocomplete suggestions"
+//             loadData(filteredData, exerciseListElement);
+//         }
+
+//     });
+    
+
+//     // clear list of autocomplete suggestions
+//     function removeElements(element) {
+//         element.innerHTML = "";
+//     }
 });
 
 
